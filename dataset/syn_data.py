ae91f9f1d3a5954041f9f70291e4bef7af6484eb
jqshang <charles.jq.shang@gmail.com>
2025-11-24 21:39:50 -0500

    update syn data

--- dataset/syn_data.py
+++ dataset/syn_data.py
@@ -20,10 +20,10 @@ def generate_synthetic_protein_data(num_acids, num_steps, pairs, seed=42):
     for t in range(num_steps - 1):
         # step 1: 先让所有人根据自身上一时刻状态进行更新 (自我回归)
         # 这一步至关重要，保证了数据有“记忆性”，而不是白噪声
-        pos[t +
-            1] = pos[t] + np.random.normal(0, noise_level, size=(num_acids, 3))
-        ang[t +
-            1] = ang[t] + np.random.normal(0, noise_level, size=(num_acids, 2))
+        pos[t + 1] = pos[
+            t] + 1  # np.random.normal(0, noise_level, size=(num_acids, 3))
+        ang[t + 1] = ang[
+            t] + 1  # np.random.normal(0, noise_level, size=(num_acids, 2))
 
         # step 2: 遍历 pairs，叠加因果效应 (使用 +=)
         for Ai, Aj in pairs:
